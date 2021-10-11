74e8d7a92da1081b987927737c8d801cb61cff0c
rufengchen <rrchen6-c@my.cityu.edu.hk>
2021-10-12 00:47:32 +0800

    limit

--- lab2.py
+++ lab2.py
@@ -13,7 +13,14 @@ print(expr + 1)
 # remove the x in the expression
 print(expr - x)
 
-# 
+#
 expanded_expr = expand(x*expr)
 print(expanded_expr)
 
+## Limit questions in lab2
+# 1.limit of 1/x, x to 1
+print(limit(1/x,x,1))
+# 2.limit of 1/x, x to 0
+print(limit(1/x,x,0))
+# 3.limit of (x^2-1)/(x+1), x to -1
+print(limit((x**2-1)/(x+1),x,-1))
